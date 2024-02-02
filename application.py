b3b7e8f6dab0520309ef9811e6a1076e6fab845a
Fahad <ftkhan61814@gmail.com>
2024-02-02 11:27:16 +0530

    done

--- application.py
+++ application.py
@@ -1,30 +1,31 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-import logging 
+import logging
 import csv
 import os
+import pandas as pd
 
 logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
-import ssl #bypassing ssl connection
+import ssl  # bypassing ssl connection
 ssl._create_default_https_context = ssl._create_unverified_context
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
 application = Flask(__name__)
 app = application
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods=['GET'])
 def homepage():
     return render_template("index.html")
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+@app.route("/review", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ", "")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             url_client = uReq(flipkart_url)
             flipkartPage = url_client.read()
@@ -35,44 +36,37 @@ def index():
             box = bigboxes[0]
             product_link = "https://www.flipkart.com" + box.div.div.div.a['href']
             product_req = requests.get(product_link)
-            product_req.encoding='utf-8'
+            product_req.encoding = 'utf-8'
             prod_html = bs(product_req.text, "html.parser")
             print(prod_html)
             comment_box = prod_html.find_all('div', {'class': "_16PBlm"})
 
             filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            
+            header = ["Product", "Customer Name", "Rating", "Heading", "Comment"]
+
+            df = pd.DataFrame(columns=header)
+
             reviews = []
             for i in comment_box:
                 try:
-                    #name.encode(encoding='utf-8')
                     name = i.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
-
                 except:
                     logging.info("name")
 
                 try:
-                    #rating.encode(encoding='utf-8')
                     rating = i.div.div.div.div.text
-
-
                 except:
                     rating = 'No Rating'
                     logging.info("rating")
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
                     commentHead = i.div.div.div.p.text
-
                 except:
                     commentHead = 'No Comment Heading'
                     logging.info(commentHead)
+
                 try:
                     comtag = i.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
                     logging.info(e)
@@ -80,17 +74,18 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-                
+
+            df = pd.DataFrame(reviews)
+            df.to_csv(filename, index=False)
+
             logging.info("log my final result {}".format(reviews))
-            return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+            return render_template('result.html', reviews=reviews[0:(len(reviews) - 1)])
         except Exception as e:
             logging.info(e)
             return 'something is wrong'
-    # return render_template('results.html')
-
     else:
         return render_template('index.html')
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=False,port=8501)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False, port=8501)
